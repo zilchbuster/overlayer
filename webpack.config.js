94769eb451392458b12934fa748b86f7b998a7f2
zilchbuster <zaid.alkhishman+1@gmail.com>
2017-09-19 22:51:59 -0400

    Add config for react/ts/webpack frontend

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,24 @@
-var path = require('path');
-
-var node_dir = __dirname + '/node_modules';
-
 module.exports = {
-    entry: './src/main/js/app.js',
-    devtool: 'sourcemaps',
-    cache: true,
-    debug: true,
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                query: {
-                    cacheDirectory: true,
-                    presets: ['es2015', 'react']
-                }
-
-            }
-        ]
-    }
-};
+  entry: 'src/main/ts/index.tsx',
+  devtool: 'sourcemaps',
+  cache: true,
+  debug: true,
+  output: {
+    path: __dirname,
+    filename: './src/main/resources/static/built/bundle.js'
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    loaders: [
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }    	
+    ]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM",
+    "rest": "Rest"
+  }
+}
